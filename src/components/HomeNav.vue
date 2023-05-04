61f7bad925469062ff7e20a18c301ef4ed170785
Tim Finley <tfinley@pop-os.localdomain>
2023-05-04 01:04:24 -0400

    tweaked links

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -8,18 +8,18 @@
         AT-9592
       </span>
     </button>
-    <button class="btn bg-accent4">
+    <a href="https://fgtech.finley-group.com" target="blank" class="btn bg-accent4">
       <span class="btn-hidden-text">
         tech
       </span>
       <span class="btn-display-text">TC-4928</span>
-    </button>
-    <button class="btn bg-secondary">
+    </a>
+    <a href="https://blog.finley-group.com" target="blank" class="btn bg-secondary">
       <span class="btn-hidden-text">
         blog
       </span>
       <span class="btn-display-text">BL-8475</span>
-    </button>
+    </a>
     <button class="btn bg-accent2" @click="$emit('comms')">
       <span class="btn-hidden-text">
         comm
