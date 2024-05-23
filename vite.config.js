395a982ab9b680c9a97f5d0110f5567954535bda
Tim Finley <tfinley@pop-os.localdomain>
2024-05-23 03:10:47 -0400

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import md from 'unplugin-vue-markdown/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    md()
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  }
+  },
 })
